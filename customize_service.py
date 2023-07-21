1e68fd62afbc0ea97ac982a9e95e93bf031166a5
Romulus <jianghy_hz@foxmail.com>
2023-07-21 19:02:27 +0800

    111111

--- customize_service.py
+++ customize_service.py
@@ -22,104 +22,6 @@ except:
 MODEL_NUM = 1232
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--task_name", default="cner", type=str,
-                        help="The name of the task to train selected in the list: ")
-    parser.add_argument("--data_dir", default="./datasets/cner/", type=str,
-                        help="The input data dir. Should contain the training files for the CoNLL-2003 NER task.", )
-    parser.add_argument("--model_type", default="bert", type=str,
-                        help="Model type selected in the list: ")
-    parser.add_argument("--model_name_or_path", default="./prev_trained_model/bert-base-chinese", type=str,
-                        help="Path to pre-trained model or shortcut name selected in the list: ")
-    parser.add_argument("--output_dir", default="./outputs/cner_output/bert/", type=str,
-                        help="The output directory where the model predictions and checkpoints will be written.", )
-
-    # Other parameters
-    parser.add_argument('--markup', default='bios', type=str,
-                        choices=['bios', 'bio'])
-    parser.add_argument('--loss_type', default='ce', type=str,
-                        choices=['lsr', 'focal', 'ce'])
-    parser.add_argument("--config_name", default="", type=str,
-                        help="Pretrained config name or path if not the same as model_name")
-    parser.add_argument("--tokenizer_name", default="", type=str,
-                        help="Pretrained tokenizer name or path if not the same as model_name", )
-    parser.add_argument("--cache_dir", default="", type=str,
-                        help="Where do you want to store the pre-trained models downloaded from s3", )
-    parser.add_argument("--train_max_seq_length", default=128, type=int,
-                        help="The maximum total input sequence length after tokenization. Sequences longer "
-                             "than this will be truncated, sequences shorter will be padded.", )
-    parser.add_argument("--eval_max_seq_length", default=512, type=int,
-                        help="The maximum total input sequence length after tokenization. Sequences longer "
-                             "than this will be truncated, sequences shorter will be padded.", )
-    parser.add_argument("--do_train", action="store_true",
-                        help="Whether to run training.")
-    parser.add_argument("--do_eval", action="store_true",
-                        help="Whether to run eval on the dev set.")
-    parser.add_argument("--do_predict", action="store_true",
-                        help="Whether to run predictions on the test set.")
-    parser.add_argument("--evaluate_during_training", action="store_true",
-                        help="Whether to run evaluation during training at each logging step.", )
-    parser.add_argument("--do_lower_case", action="store_true",
-                        help="Set this flag if you are using an uncased model.")
-    # adversarial training
-    parser.add_argument("--do_adv", action="store_true",
-                        help="Whether to adversarial training.")
-    parser.add_argument('--adv_epsilon', default=1.0, type=float,
-                        help="Epsilon for adversarial.")
-    parser.add_argument('--adv_name', default='word_embeddings', type=str,
-                        help="name for adversarial layer.")
-
-    parser.add_argument("--per_gpu_train_batch_size", default=8, type=int,
-                        help="Batch size per GPU/CPU for training.")
-    parser.add_argument("--per_gpu_eval_batch_size", default=8, type=int,
-                        help="Batch size per GPU/CPU for evaluation.")
-    parser.add_argument("--gradient_accumulation_steps", type=int, default=1,
-                        help="Number of updates steps to accumulate before performing a backward/update pass.", )
-    parser.add_argument("--learning_rate", default=5e-5, type=float,
-                        help="The initial learning rate for Adam.")
-    parser.add_argument("--crf_learning_rate", default=5e-5, type=float,
-                        help="The initial learning rate for crf and linear layer.")
-    parser.add_argument("--weight_decay", default=0.01, type=float,
-                        help="Weight decay if we apply some.")
-    parser.add_argument("--adam_epsilon", default=1e-8, type=float,
-                        help="Epsilon for Adam optimizer.")
-    parser.add_argument("--max_grad_norm", default=1.0, type=float,
-                        help="Max gradient norm.")
-    parser.add_argument("--num_train_epochs", default=3.0, type=float,
-                        help="Total number of training epochs to perform.")
-    parser.add_argument("--max_steps", default=-1, type=int,
-                        help="If > 0: set total number of training steps to perform. Override num_train_epochs.", )
-
-    parser.add_argument("--warmup_proportion", default=0.1, type=float,
-                        help="Proportion of training to perform linear learning rate warmup for,E.g., 0.1 = 10% of training.")
-    parser.add_argument("--logging_steps", type=int, default=50,
-                        help="Log every X updates steps.")
-    parser.add_argument("--save_steps", type=int, default=50, help="Save checkpoint every X updates steps.")
-    parser.add_argument("--eval_all_checkpoints", action="store_true",
-                        help="Evaluate all checkpoints starting with the same prefix as model_name ending and ending with step number", )
-
-    # 这里改模型号码
-    parser.add_argument("--predict_checkpoints", type=int, default=MODEL_NUM,
-                        help="predict checkpoints starting with the same prefix as model_name ending and ending with step number")
-
-    parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
-    parser.add_argument("--overwrite_output_dir", action="store_true",
-                        help="Overwrite the content of the output directory")
-    parser.add_argument("--overwrite_cache", action="store_true",
-                        help="Overwrite the cached training and evaluation sets")
-    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
-    parser.add_argument("--fp16", action="store_true",
-                        help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit", )
-    parser.add_argument("--fp16_opt_level", type=str, default="O1",
-                        help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
-                             "See details at https://nvidia.github.io/apex/amp.html", )
-    parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
-    parser.add_argument("--server_ip", type=str, default="", help="For distant debugging.")
-    parser.add_argument("--server_port", type=str, default="", help="For distant debugging.")
-    return parser
-
-
 def read_data(filepath):
     sentences = []
     sent = ['[START]']
@@ -165,8 +67,6 @@ class CustomizeService(PTServingBaseService):
         res = {"result": []}
         words = []
         line = []
-        # print("!!!!!!!!!!!!!!!!!!!!")
-        # print(os.path.join(self.code_url, "datasets/cner/test.char.bmes"))
         with open(os.path.join(self.code_url, "datasets/cner/test.char.bmes"), "r", encoding="utf-8") as f:
             for i in f.readlines():
                 if i != "\n":
@@ -210,7 +110,8 @@ class CustomizeService(PTServingBaseService):
 
     def __init__(self, model_name, model_path):  # model_name, model_path 没用
         self.code_url = os.path.dirname(os.path.abspath(__file__))
-        self.args = get_args().parse_args()
+        self.args = argparse.Namespace()
+        print(type(self.args))
         MODEL_CLASSES = {
             'bert': (BertConfig, BertCrfForNer, BertTokenizer),
         }
@@ -220,11 +121,25 @@ class CustomizeService(PTServingBaseService):
         self.args.data_dir = os.path.join(self.code_url, "datasets/cner/")
         self.args.model_name_or_path = os.path.join(self.code_url, "prev_trained_model/bert-base-chinese")
         self.args.output_dir = os.path.join(self.code_url, "outputs/cner_output/bert/")
-
         self.args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.args.do_eval = False
         self.args.do_train = False
         self.args.do_predict = True
+        self.args.do_lower_case = False
+        self.args.predict_checkpoints = MODEL_NUM
+        self.args.task_name = "cner"
+        self.args.model_type = "bert"
+        self.args.markup = "bios"
+        self.args.local_rank = -1
+        self.args.overwrite_cache = False
+        self.args.eval_max_seq_length = 512
+        self.args.train_max_seq_length = 128
+        self.args.gradient_accumulation_steps = 1
+        self.args.logging_steps = 50
+        self.args.warmup_proportion = 0.1
+        self.args.seed = 42
+        self.args.fp16_opt_level = "O1"
+        self.args.max_steps = -1
         processor = CnerProcessor()
         label_list = processor.get_labels()
         self.args.id2label = {i: label for i, label in enumerate(label_list)}
@@ -256,13 +171,11 @@ class CustomizeService(PTServingBaseService):
         for checkpoint in checkpoints:
             prefix = checkpoint.split('/')[-1] if checkpoint.find('checkpoint') != -1 else ""
             model = self.model_class.from_pretrained(checkpoint, config=config)
-            # device = prepare_device()
             model.to(self.args.device)
             predict(self.args, model, tokenizer, prefix=prefix)
         return data
 
     def _postprocess(self, data) -> dict:
         # 输出格式是{"result":["x O\n","x B-DATE\n"]}
-        logger.info("in postprocess")
         data = self.get_res()
         return data
